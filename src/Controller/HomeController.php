93127dada1660d7c9dc75b8fc21069be37b1d9a9
kenjy <kenjyzakary@hotmail.com>
2021-02-04 16:01:11 +0100

    formulaire_login_html

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Product;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
@@ -18,5 +19,28 @@ class HomeController extends AbstractController
     public function index()
     {
         return $this->render('home.html.twig');
+    }
+
+    /**
+     * @Route("/add-product", name="add-product")
+     */
+    public function addProduct(Request $request)
+    {
+        $new_product = new Product;
+        $form = $this->createForm(ProductType::class, $new_product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($new_product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('product.html.twig', [
+            "form" => $form->createView()
+        ]);
     }
 }
