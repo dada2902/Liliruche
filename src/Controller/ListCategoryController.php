1020ab50c662c3ce88feef8a5d50c5dfa52c4a55
kenjy <kenjyzakary@hotmail.com>
2021-02-13 08:01:33 +0100

    en cour de fonctinnement

--- src/Controller/ListCategoryController.php
+++ src/Controller/ListCategoryController.php
@@ -14,19 +14,48 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ListCategoryController extends AbstractController
 {
-    
-  /**
-   * @Route("/affichage-category-home", name="affichage-category-home")
-   */
 
-  public function affichageListCategory()
+  /**
+   * @Route("/info/miel", name="miel")
+   */
+  public function miel()
   {
-      
-      $categories = $this->getDoctrine()->getRepository(Category::class)->findall();
-      return $this->render('home.html.twig', [
-         'categories' => $categories
-        
-      ]);
+    $products = $this->getDoctrine()->getRepository(Product::class)->findAll();
+    return $this->render('listCategory/miel.html.twig', [
+      'products' => $products
+    ]);
   }
 
+  /**
+   * @Route("/info/epiceriefine", name="epiceriefine")
+   */
+  public function epiceriefine()
+  {
+    $products = $this->getDoctrine()->getRepository(Product::class)->findAll();
+    return $this->render('listCategory/epiceriefine.html.twig', [
+      'products' => $products
+    ]);
+  }
+
+  /**
+   * @Route("/info/coffretscadeaux", name="coffretscadeaux")
+   */
+  public function coffretscadeaux()
+  {
+    $products = $this->getDoctrine()->getRepository(Product::class)->findAll();
+    return $this->render('listCategory/coffretscadeaux.html.twig', [
+      'products' => $products
+    ]);
+  }
+
+  /**
+   * @Route("/info/bienetre", name="bienetre")
+   */
+  public function bienetre()
+  {
+    $products = $this->getDoctrine()->getRepository(Product::class)->findAll();
+    return $this->render('listCategory/bienetre.html.twig', [
+      'products' => $products
+    ]);
+  }
 }
